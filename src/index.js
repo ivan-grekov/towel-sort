6c56dfafdd523b56eca1bd7c5a713ed6740e1a65
Ivan <ivan_grekov@yahoo.com>
2021-08-28 14:12:28 +0300

    feat: add script for snake_sort

--- src/index.js
+++ src/index.js
@@ -2,5 +2,17 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  let snakeSortArr = [];
+    if (!matrix || matrix.length === 0) {
+      return [];
+    }
+    for (let i = 0; i < matrix.length; i++) {
+      for (let j = 0; j < matrix[i].length; j++) {
+        let columnIndex = i % 2 === 0 
+        ? j
+        : (matrix[i].length - j - 1);
+        snakeSortArr.push(matrix[i][columnIndex]);
+      }
+    }
+  return snakeSortArr;
 }
